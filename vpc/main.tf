a3b0dbf6e5066626479e1e819248bca071b9618a
Joseph Ligier <joseph.ligier@accenture.com>
2021-03-17 07:58:13 +0000

    Add a Nat gateway

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,3 +9,29 @@ resource "huaweicloud_vpc_subnet" "this" {
   gateway_ip = var.gateway_ip
   vpc_id     = huaweicloud_vpc.this.id
 }
+
+resource "huaweicloud_nat_gateway" "this" {
+  name                = "Terraform"
+  description         = "test for terraform"
+  spec                = "1"
+  router_id           = huaweicloud_vpc.this.id
+  internal_network_id = huaweicloud_vpc_subnet.this.id
+}
+
+resource "huaweicloud_vpc_eip" "this" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name        = "test"
+    size        = 1
+    share_type  = "PER"
+    charge_mode = "traffic"
+  }
+}
+
+resource "huaweicloud_nat_snat_rule" "this" {
+  floating_ip_id = huaweicloud_vpc_eip.this.id
+  nat_gateway_id = huaweicloud_nat_gateway.this.id
+  network_id     = huaweicloud_vpc_subnet.this.id
+}
